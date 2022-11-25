aef488780cc9b1d86fab605bd141f65a28c24417
Suliman <sssuliman@bk.ru>
2022-11-26 00:54:21 +0300

    Сделал адаптивную верстку

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const telElement = document.getElementById('tel') // ищем наш единственный input
-    const innElement = document.getElementById('inn')
-    const maskOptions = { // создаем объект параметров
-      mask: '+{7}(000)000-00-00' // задаем единственный параметр mask
-    }
-    const innMask = {
-      mask: '000000000000'
-    }
-    IMask(telElement, maskOptions) // запускаем плагин с переданными параметрами
-    IMask(innElement, innMask) // запускаем плагин с переданными параметрами
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const telElement = document.getElementById("tel"); // ищем наш единственный input
+  const innElement = document.getElementById("inn");
+  const maskOptions = {
+    // создаем объект параметров
+    mask: "+{7}(000)000-00-00", // задаем единственный параметр mask
+  };
+  const innMask = {
+    mask: "000000000000",
+  };
+  IMask(telElement, maskOptions); // запускаем плагин с переданными параметрами
+  IMask(innElement, innMask); // запускаем плагин с переданными параметрами
+});
+
+let description = document.getElementsByClassName("prod__header");
+
+if (document.documentElement.clientWidth < 531) {
+  for (let i in description) {
+    description[i].innerHTML = description[i].innerText.slice(0, 44) + "...";
+  }
+}
